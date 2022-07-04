092845dc5135cc2640acfa559059d0ed16383bdb
MunyeolLee <MunyeolLee>
2022-07-04 22:22:36 +0900

    User 엔티티 추가

---
+++ src/main/java/com/demo/board/domain/user/entity/User.java
@@ -0,0 +1,49 @@
+package com.demo.board.domain.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String userId;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime modifiedDate;
+
+    @Builder
+    public User(String userId, String password, String name) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.createdDate = LocalDateTime.now();
+    }
+
+    public void update(String password, String name) {
+        this.password = password;
+        this.name = name;
+        this.modifiedDate = LocalDateTime.now();
+    }
+
+}
