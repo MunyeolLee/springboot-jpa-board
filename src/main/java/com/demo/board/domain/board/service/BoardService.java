03c4134c164d85a948a3c0abf7dfea4a250a1c46
MunyeolLee <MunyeolLee>
2022-07-05 16:49:32 +0900

    Board 서비스 생성
    save : 게시글 저장
    update : 게시글 수정
    findAll : 전체 게시글 목록 조회
    findById : 게시글 상세 조회

---
+++ src/main/java/com/demo/board/domain/board/service/BoardService.java
@@ -0,0 +1,58 @@
+package com.demo.board.domain.board.service;
+
+import com.demo.board.api.board.dto.BoardRequestDto;
+import com.demo.board.api.board.dto.BoardResponseDto;
+import com.demo.board.domain.board.entity.Board;
+import com.demo.board.domain.board.repository.BoardRepository;
+import com.demo.board.global.exception.BusinessException;
+import com.demo.board.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    /*
+     * 게시글 저장
+     */
+    @Transactional
+    public Long save(BoardRequestDto params) {
+        Board board = boardRepository.save(params.toEntity());
+        return board.getId();
+    }
+
+    /*
+     * 게시글 수정
+     */
+    @Transactional
+    public Long update(Long id, BoardRequestDto params) {
+        Board board = boardRepository.findById(id).orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND));
+        board.update(params.getTitle(), params.getContent());
+        return id;
+    }
+
+    /*
+     * 전체 게시글 목록 조회
+     */
+    public List<BoardResponseDto> findAll() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdDate");
+        List<Board> boardList = boardRepository.findAll(sort);
+        return boardList.stream().map(BoardResponseDto::new).collect(Collectors.toList());
+    }
+
+    /*
+     * 게시글 상세 조회
+     */
+    public BoardResponseDto findById(Long id) {
+        Board board = boardRepository.findById(id).orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND));
+        return new BoardResponseDto(board);
+    }
+}
