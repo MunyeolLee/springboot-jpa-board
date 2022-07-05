0497f1491b2b725cc4b61aed3bd5aa218caab83d
MunyeolLee <MunyeolLee>
2022-07-05 11:19:32 +0900

    Board 엔티티 추가

---
+++ src/main/java/com/demo/board/domain/board/entity/Board.java
@@ -0,0 +1,46 @@
+package com.demo.board.domain.board.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    @Lob
+    private String content;
+
+    @Column(nullable = false)
+    private int views;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    @Builder
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.views = 0;
+        this.createdDate = LocalDateTime.now();
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.modifiedDate = LocalDateTime.now();
+    }
+}
