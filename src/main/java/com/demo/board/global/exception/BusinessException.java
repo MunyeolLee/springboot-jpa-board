f07af5cafbe1fdf1cde9d832c9d07ca676a887e9
MunyeolLee <MunyeolLee>
2022-07-05 10:59:04 +0900

    비지니스 예외 클래스 추가

---
+++ src/main/java/com/demo/board/global/exception/BusinessException.java
@@ -0,0 +1,20 @@
+package com.demo.board.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private int status;
+
+    public BusinessException(int status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.status = errorCode.getStatus();
+    }
+
+}
