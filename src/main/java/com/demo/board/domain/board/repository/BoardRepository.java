46776c4eda11967f20bc601208c22b7b70ad20eb
MunyeolLee <MunyeolLee>
2022-07-10 14:03:57 +0900

    게시글 조회 - 검색 기능 구현

--- src/main/java/com/demo/board/domain/board/repository/BoardRepository.java
+++ src/main/java/com/demo/board/domain/board/repository/BoardRepository.java
@@ -1,9 +1,21 @@
 package com.demo.board.domain.board.repository;
 
 import com.demo.board.domain.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitleContainingOrContentContaining(String title, String content, Sort sort);
+
+    List<Board> findByTitleContaining(String title, Sort sort);
+
+    List<Board> findByContentContaining(String content, Sort sort);
+
 }
