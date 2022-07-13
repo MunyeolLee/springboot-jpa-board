4bba786c33941ec7282112a6c7be3e64f9969f87
MunyeolLee <MunyeolLee>
2022-07-13 21:48:04 +0900

    로그인 처리를 위한 메소드 추가
    로그인 기능은 아직 미완

--- src/main/java/com/demo/board/domain/user/service/UserService.java
+++ src/main/java/com/demo/board/domain/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.demo.board.domain.user.service;
 
+import com.demo.board.api.login.dto.LoginRequestDto;
+import com.demo.board.api.login.dto.SignupRequestDto;
 import com.demo.board.domain.user.entity.User;
 import com.demo.board.domain.user.repository.UserRepository;
 import com.demo.board.global.exception.BusinessException;
 import com.demo.board.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -12,22 +15,34 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
 
     @Transactional
-    public Long save(User user) {
+    public Long save(SignupRequestDto params) {
         // UserId 로 유저 조회
-        Optional<User> findUser = userRepository.findByUserId(user.getUserId());
+        Optional<User> findUser = userRepository.findByUserId(params.getUserId());
         if( findUser.isPresent() ) {
             // 조회된 유저가 있으면 오류 발생
-            new BusinessException(ErrorCode.ALREADY_REGISTERED_USER);
+            throw new BusinessException(ErrorCode.ALREADY_REGISTERED_USER);
         }
 
         // 유저 정보 저장
-        User save = userRepository.save(user);
+        User save = userRepository.save(params.toEntity());
+
         return save.getId();
     }
 
+    public User login(LoginRequestDto params) {
+        // UserId 유저 조회
+        Optional<User> findUser = userRepository.findByUserId(params.getUserId());
+        if( findUser.isEmpty() ) {
+            // 조회된 유저가 없으면 오류 발생
+            throw new BusinessException(ErrorCode.CAN_NOT_FOUND_USER);
+        }
+
+        return findUser.get();
+    }
 }
