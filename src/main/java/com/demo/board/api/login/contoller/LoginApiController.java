c8f231f3eff65c055d2316ee7a2a53f140ceb245
MunyeolLee <MunyeolLee>
2022-07-13 21:45:22 +0900

    상태코드를 HttpsStatus 객체에서 사용하도록 수정
    StatusCode 제거하고
    HttpStatus 이용

--- src/main/java/com/demo/board/api/login/contoller/LoginApiController.java
+++ src/main/java/com/demo/board/api/login/contoller/LoginApiController.java
@@ -3,8 +3,8 @@ package com.demo.board.api.login.contoller;
 import com.demo.board.api.login.dto.SignupRequestDto;
 import com.demo.board.domain.user.service.UserService;
 import com.demo.board.global.response.ResponseObject;
-import com.demo.board.global.response.StatusCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +24,7 @@ public class LoginApiController {
 
         return ResponseEntity.ok().body(
                 ResponseObject.builder()
-                        .status(StatusCode.OK.getStatus())
+                        .status(HttpStatus.OK.value())
                         .message("회원가입이 완료 되었습니다.")
                         .data("")
                         .build()
