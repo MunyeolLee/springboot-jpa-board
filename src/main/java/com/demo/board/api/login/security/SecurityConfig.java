7f9779af4fd48b1f9d6473073095b1dbe7d60435
MunyeolLee <MunyeolLee>
2023-03-22 23:33:36 +0900

    Deprecated WebSecurityConfigurerAdapter 제거

--- src/main/java/com/demo/board/api/login/security/SecurityConfig.java
+++ src/main/java/com/demo/board/api/login/security/SecurityConfig.java
@@ -2,33 +2,21 @@ package com.demo.board.api.login.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@RequiredArgsConstructor
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsServiceImpl userDetailsService;
+@Configuration
+public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder encodePWD() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(encodePWD());
-    }
-
-    @Override
-    protected  void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
                 .authorizeRequests() // 인가 요청이 오면
 //                .antMatchers("/auth/*", "/css/**/*","/fonts/**/*","/plugins/**/*","/scripts/**/*") // 해당 경로들은
                 .antMatchers("/**/*") // 회원가입 기능 구현 전까지 패스..
@@ -38,10 +26,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and() // 그리고 ..
                 .formLogin()
                 .loginProcessingUrl("/auth/login") // 해당 주소로 로그인 요청을 하면 스프링 시큐리티가 가로채서 로그인을 해준다.
-                .defaultSuccessUrl("/board/list"); // 로그인 완료 후 이동 주소
+                .defaultSuccessUrl("/board/list") // 로그인 완료 후 이동 주소
+                .and().build();
+    }
 
-        httpSecurity.sessionManagement()
-                .maximumSessions(1) // 세션 최대 허용 수
-                .maxSessionsPreventsLogin(false); // false : 중복 로그인하면 이전 로그인은 무효
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
